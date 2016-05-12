9405679dfaf752b61d117a62faaad6c47dac414b
Colby Fayock <fayock@gmail.com>
2016-05-11 21:17:26 -0400

    404

--- content/themes/wp-init/404.php
+++ content/themes/wp-init/404.php
@@ -1,6 +1,21 @@
 <div class="row">
-    <div class="twelvecol">
-        <h1>404</h1>
-        <h2>Page not found!</h2>
+    <div class="eightcol center">
+
+        <article id="post-<? the_ID(); ?>" class="row" role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+            <header class="twelvecol article-header">
+
+                <h1 class="page-title align-center push-bottom" itemprop="headline">
+                    404!!
+                </h1>
+
+            </header>
+
+            <section class="twelvecol entry-content cf align-center" itemprop="articleBody">
+                <img src="http://fay.io/content/uploads/2016/05/vader.jpg">
+            </section>
+
+        </article>
+
     </div>
 </div>
